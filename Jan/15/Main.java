f5ac7afba3149099dfe44288964817ab23384ae5
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-01-15 23:25:21 +0300

    15

---
+++ Jan/15/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 12;
+        Solution solution = new Solution();
+        int result = solution.minimizeXor(num1, num2);
+        System.out.println(result);
+    }
+}
