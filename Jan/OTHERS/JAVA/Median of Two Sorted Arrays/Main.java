91c2e2c7f8313ba4eee2246e1dbcf5ed5c5276f1
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-01-15 23:36:06 +0300

    Median of Two Sorted Arrays

---
+++ Jan/OTHERS/JAVA/Median of Two Sorted Arrays/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        Solution solution = new Solution();
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+    }
+}
