7e0c09d23f4f8246d003c2ddd17e48804da28061
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-01-17 21:05:45 +0300

    17

---
+++ Jan/17/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] derived = {1, 1 , 0};
+        Solution sol = new Solution();
+        System.out.println(sol.doesValidArrayExist(derived));
+    }
+}
