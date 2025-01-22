f214db154f14a723547169fa0687e52f2f263919
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-01-22 18:45:55 +0300

    Text Justification

---
+++ Interviews/Text Justification/main.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i % (len(cur) - 1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
+        
+
+if __name__ == '__main__':
+    words = ["This", "is", "an", "example", "of", "text", "justification."]
+    maxWidth = 16
+    print(Solution().fullJustify(words, maxWidth))
