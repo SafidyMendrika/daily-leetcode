609937a6350fd55f82c5f2515a9632503870ff81
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-02-13 18:12:12 +0300

    13

---
+++ Feb/13/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int [] nums = {2,11,10,1,3};
+        int k = 10;
+
+        System.out.println(solution.minOperations(nums, k));
+    }
+}
---
+++ Feb/13/Solution.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        
+        for (int num : nums) {
+            pq.offer((long) num);
+        }
+        
+        int operations = 0;
+        long x = 0,y=0, newValue = 0;
+        while (pq.size() >= 2 && pq.peek() < k) {
+            x = pq.poll();
+            y = pq.poll();
+            
+            newValue = Math.min(x, y) * 2 + Math.max(x, y);
+            
+            pq.offer(newValue);
+            
+            operations++;
+        }
+        
+        if (pq.peek() < k) {
+            return -1;
+        }
+        
+        return operations;
+    }
+}
