4742c181a2f6d9670518ee924da678efa0095465
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-02-28 21:23:26 +0300

    28

---
+++ Feb/28/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "abac";
+        String str2 = "cab";
+        Solution s = new Solution();
+
+        System.out.println(s.shortestCommonSupersequence(str1, str2));
+    }
+}
