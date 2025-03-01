f17d1474f613069375cdd9629aa9891d03866cb4
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-01 10:13:25 +0300

    01

---
+++ Mar/01/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {0,1};
+        // int[] nums = {1,2,2,1,1,0};
+
+        s.displayArray(s.applyOperations(nums));
+    }
+}
