979b0f877b32e0feb73c86ecb8e6e8d800e61f2d
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-12 21:14:03 +0300

    12

---
+++ Mar/12/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {-2,-1,-1,1,2,3};
+
+        System.out.println(solution.maximumCount(nums));
+
+    }
+}
