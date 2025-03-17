937bf76670b1d15149f6beef200e26c504df68e3
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-17 21:34:26 +0300

    17

---
+++ Mar/17/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {3,2,3,2,2,2};
+
+        System.out.println(solution.divideArray(nums));
+
+    }
+}
