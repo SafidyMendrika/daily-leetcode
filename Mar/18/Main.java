04f578bf5f3f16ec6d1806beb986b7707809f77d
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-18 21:17:31 +0300

    18

---
+++ Mar/18/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1,3,8,48,10};
+
+        System.out.println(solution.longestNiceSubarray(nums));
+
+    }
+}
