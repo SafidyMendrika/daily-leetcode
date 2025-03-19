f8c5d49ec723659368044cc0db1988745b6a4999
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-19 20:58:06 +0300

    19

---
+++ Mar/19/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {0,1,1,1,0,0};
+
+        System.out.println(solution.minOperations(nums));
+
+    }
+}
