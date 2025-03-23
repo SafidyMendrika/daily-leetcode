1db17502e8023b45895d5e0c77ec7f32a8fa15eb
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-03-23 22:56:07 +0300

    23

---
+++ Mar/23/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int n = 2;
+        int[][] roads = {{1,0,10}};
+        
+        System.out.println(solution.countPaths(n, roads));
+    }
+}
