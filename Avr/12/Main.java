adf0e503165776e6c1cf9bc934830462857ee077
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-04-12 20:04:04 +0300

    12

---
+++ Avr/12/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int n = 5 ;
+        int k = 6 ;
+
+        System.out.println(solution.countGoodIntegers(n,k));
+    }   
+}
