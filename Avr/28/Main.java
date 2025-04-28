143ab83b955b40298a826949b88d82b2671de293
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-04-28 16:24:18 +0300

    28

---
+++ Avr/28/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] nums = {1,1,1};
+        int k = 5;
+
+        System.out.println(solution.countSubarrays(nums,k)); 
+
+    }   
+}
