98710514da2552661e7fa2d9bad53657abfc7233
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-05-10 20:56:56 +0300

    10

---
+++ Mai/10/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] nums1 = {3,2,0,1,0};
+        int[] nums2 = {6,5,0};
+
+        System.out.println(solution.minSum(nums1, nums2)); 
+
+    }   
+}
