1dde1dc10e187e9439f538e58ae16c93b6bb1ce2
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-05-11 20:51:57 +0300

    11

---
+++ Mai/11/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int oddCount = 0;
+
+        for(int n : arr){
+            if(n%2 !=0){
+                oddCount++;
+            }else{
+                oddCount = 0;
+            }
+
+            if(oddCount == 3) return true;
+        }
+
+        return false;
+
+    }
+}
