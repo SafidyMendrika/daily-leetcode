03935ca1c9dbd48d7b4f447cb5c5e60c83411a2b
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2025-05-19 09:16:22 +0300

    19

---
+++ Mai/19/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Solution solution = new Solution();
+        int[] nums = {3,4,5};
+        System.out.println(solution.triangleType(nums));
+        
+
+    }   
+}
