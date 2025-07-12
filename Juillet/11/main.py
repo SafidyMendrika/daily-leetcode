46844715ec95e1102542b218971d925c82001358
SafidyMendrikaLagoon <mendrikarazafimalaza@gmail.com>
2025-07-12 21:11:58 +0300

    12

--- Juillet/11/main.py
+++ Juillet/11/main.py
@@ -1,37 +1,49 @@
-import heapq
+from collections import deque
+
 class Solution:
-    def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
-        meetings.sort()
+    def earliestAndLatest(self, n: int, firstPlayer: int, secondPlayer: int) -> list[int]:
+        min_round = n
+        max_round = 0
+        queue = deque([(list(range(1, n + 1)), 1)])
+        
+        while queue:
+            arr, r = queue.pop()
 
-        free_rooms = list(range(n))
-        heapq.heapify(free_rooms)
+            if len(arr) <= 2:
+                if arr[0] == firstPlayer and arr[-1] == secondPlayer \
+                    or arr[-1] == secondPlayer and arr[0] == firstPlayer:
+                    min_round = min(min_round, r)
+                    max_round = max(max_round, r)
+                continue
 
-        busy = []
-        count = [0] * n
+            new_arr = [[]] if len(arr) % 2 == 0 else [[arr[len(arr) // 2]]]
+            for i in range(len(arr) // 2):
+                if arr[i] == firstPlayer and arr[len(arr) - i - 1] == secondPlayer \
+                or arr[i] == secondPlayer and arr[len(arr) - i - 1] == firstPlayer:
+                    min_round = min(min_round, r)
+                    max_round = max(max_round, r)
+                    continue
 
-        for start, end in meetings:
-            duration = end - start
-
-            while busy and busy[0][0] <= start:
-                _, room = heapq.heappop(busy)
-                heapq.heappush(free_rooms, room)
-
-            if free_rooms:
-                room = heapq.heappop(free_rooms)
-                count[room] += 1
-                heapq.heappush(busy, (end, room))
-            else:
-                free_time, room = heapq.heappop(busy)
-                count[room] += 1
-                heapq.heappush(busy, (free_time + duration, room))
-
-        max_meetings = max(count)
-        for i, c in enumerate(count):
-            if c == max_meetings:
-                return i
+                stack = []
+                if arr[i] == firstPlayer or arr[i] == secondPlayer:
+                    for x in new_arr:
+                        stack.append(x + [arr[i]])
+                elif arr[len(arr) - i - 1] == firstPlayer or arr[len(arr) - i - 1] == secondPlayer:
+                    for x in new_arr:
+                        stack.append(x + [arr[len(arr) - i - 1]])
+                else:
+                    for x in new_arr:
+                        stack.append(x + [arr[i]])
+                        stack.append(x + [arr[len(arr) - i - 1]])
+                new_arr = stack
             
+            for arrs in new_arr:
+                queue.append((sorted(arrs), r + 1))
+
+        return [min_round, max_round]
 
 solution = Solution()
-n = 3
-meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]
-print(solution.mostBooked(n, meetings))
+n = 11
+firstPlayer = 2
+secondPlayer = 4
+print(solution.earliestAndLatest(n, firstPlayer, secondPlayer))
