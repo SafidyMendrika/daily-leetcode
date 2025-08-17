232141c7bcd2ecf9c559887d35515bad5dc45edd
SafidyMendrikaLagoon <mendrikarazafimalaza@gmail.com>
2025-08-17 19:13:38 +0300

    17

---
+++ Aout/17/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp: list[float] = [0] * (n + 1)
+        dp[0] = 1
+        cur_prob: float = int(bool(k))
+        for i in range(1, n + 1):
+            dp[i] = cur_prob / maxPts
+            if i < k: cur_prob += dp[i]
+            if i - maxPts >= 0 and i - maxPts < k: cur_prob -= dp[i - maxPts]
+        return sum(dp[k:])
+    
+solution = Solution()
+n = 10
+k = 1
+maxPts = 10
+print(solution.new21Game(n, k, maxPts))
