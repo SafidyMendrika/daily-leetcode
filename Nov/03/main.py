2792f5919d1fe8f6b493e2618558143c3d344ad3
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2025-11-03 09:49:05 +0300

    03

---
+++ Nov/03/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        prev_c = colors[0]
+        prev_time = neededTime[0]
+        ans = 0
+        for i in range(1,n):
+            if colors[i] == prev_c:
+                ans += min(neededTime[i],prev_time)
+                prev_time = max(neededTime[i],prev_time)
+            else:
+                prev_time = neededTime[i]
+                prev_c = colors[i]
+        return ans
+    
+solution = Solution()
+colors = "abaac"
+neededTime = [1,2,3,4,5]
+print(solution.minCost(colors,neededTime))
