ebb54be98baab225ac33962eae75b710adc89e04
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2025-11-13 20:26:06 +0300

    13

---
+++ Nov/13/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        total = 0
+        ones = 0
+
+        for i in range(len(s) - 1):
+            if s[i] == "1":
+                ones += 1
+
+                if s[i+1] == "0":
+                    total += ones
+
+
+        return total
+    
+solution = Solution()
+s = "1001101"
+print(solution.maxOperations(s))
