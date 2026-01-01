a9a0fa4e771c8bb57f46d82b2d8d6adae0566952
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2026-01-01 20:50:51 +0300

    files managing and 01/01/2026

---
+++ 2026/Jan/01/main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n=len(digits)
+        
+        rem=0
+        digits[-1]+=1
+        for i in range(n-1,-1,-1):
+            Sum=rem+digits[i]
+            digits[i]=(Sum%10) 
+            rem=Sum//10
+        
+        if rem:
+            digits.insert(0,rem)
+        return digits
+
+        
+
+solution = Solution()
+digits = [1,2,3]
+print(solution.plusOne(digits))
